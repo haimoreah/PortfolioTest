d643e5ab3f4409e19644d261d1f998e891050281
haimoreah <122688733+haimoreah@users.noreply.github.com>
2026-07-22 10:35:33 +0000

    fix: update Instagram link to mr_amwal

--- src/lib/brand.ts
+++ src/lib/brand.ts
@@ -1,2 +1,2 @@
 export const BRAND_NAME = "Mr_Amwal";
-export const PERSONAL_PAGE_URL = "https://www.instagram.com/mr_haimore/";
+export const PERSONAL_PAGE_URL = "https://www.instagram.com/mr_amwal/";
